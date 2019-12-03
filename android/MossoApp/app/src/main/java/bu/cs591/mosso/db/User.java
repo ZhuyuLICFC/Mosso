22f807a421f12a1ad253f3daf99da415eea12edf
Qinmei Du <duqinmei@bu.edu>
2019-12-03 15:39:40 -0500

    Update User.java

--- android/MossoApp/app/src/main/java/bu/cs591/mosso/db/User.java
+++ android/MossoApp/app/src/main/java/bu/cs591/mosso/db/User.java
@@ -2,14 +2,21 @@ package bu.cs591.mosso.db;
 
 import android.net.Uri;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    public String userID;
     public String name;
     public String email;
     public Uri photoUrl;
+    public List<User> friends;
 
     public User(String n, String mail, Uri photo) {
         name = n;
         email = mail;
         photoUrl = photo;
+        userID = "";
+        friends = new ArrayList<>();
     }
 }
