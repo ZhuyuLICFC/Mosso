945648029726bd43d3c2690dfa538a4323bee00c
br3fc00l <461761446@qq.com>
2019-12-07 20:02:04 -0500

    update friends view model

--- android/MossoApp/app/src/main/java/bu/cs591/mosso/ui/friend/FriendViewModel.java
+++ android/MossoApp/app/src/main/java/bu/cs591/mosso/ui/friend/FriendViewModel.java
@@ -1,10 +1,15 @@
 package bu.cs591.mosso.ui.friend;
 
+import java.util.List;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import bu.cs591.mosso.db.User;
 
 public class FriendViewModel extends ViewModel {
+
+    private List<User> friends;
 
     private MutableLiveData<String> mText;
 
